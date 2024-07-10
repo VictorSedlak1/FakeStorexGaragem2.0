0f78edb8b4ef45b3390a3d4ae252268da3fca6bb
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-07-10 16:54:12 -0300

    Desing login

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b0cgbe1osv8"
+    "revision": "0.s75pf411i5o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
+<template>
+  <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth
+      app-id="sPp5QqCd0rfXxLZcwEe8aAny"
+    ></passage-auth>
+  </div>
+</template>
+
+<style></style>
